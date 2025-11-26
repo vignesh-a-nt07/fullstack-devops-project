b895b39a842e8d9e26684fe33034ba95a536cf7c
vignesh-a-nt07 <vignesh.a@navattech.com>
2025-11-26 16:52:30 +0530

    backend

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -5,3 +5,11 @@ output "frontend_public_ip" {
 output "backend_private_ip" {
   value = aws_instance.backend.private_ip
 }
+
+output "backend_instance_id" {
+  value = aws_instance.backend.id
+}
+
+output "frontend_instance_id" {
+  value = aws_instance.frontend.id
+}
