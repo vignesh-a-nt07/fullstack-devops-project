e903e919b342c837d13ee8e2c4a2ce337d4e1d88
vignesh-a-nt07 <vignesh.a@navattech.com>
2025-11-26 16:34:49 +0530

    backend

--- terraform/modules/alb/outputs.tf
+++ terraform/modules/alb/outputs.tf
@@ -1,1 +1,3 @@
-output "dns_name" { value = aws_lb.alb.dns_name }
+output "alb_dns_name" {
+  value = aws_lb.alb.dns_name
+}
--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -1,2 +1,7 @@
-output "endpoint" { value = aws_db_instance.postgres.address }
-output "port" { value = aws_db_instance.postgres.port }
+output "rds_endpoint" {
+  value = aws_db_instance.postgres.address
+}
+
+output "port" { 
+    value = aws_db_instance.postgres.port
+}
