f02d212d970174dc244ae8f9858df954499a7efd
vignesh-a-nt07 <vignesh.a@navattech.com>
2025-11-26 16:46:18 +0530

    backend

--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -2,7 +2,6 @@ output "rds_endpoint" {
   value = aws_db_instance.postgres.address
 }
 
-
 output "port" { 
     value = aws_db_instance.postgres.port
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,19 @@
-output "frontend_public_ip" { value = module.ec2.frontend_public_ip }
-output "backend_private_ip" { value = module.ec2.backend_private_ip }
-output "alb_dns_name" { value = module.alb.dns_name }
-output "rds_endpoint" { value = module.rds.endpoint }
-output "key_name" { value = aws_key_pair.deployer.key_name }
+output "frontend_public_ip" {
+  value = module.ec2.frontend_public_ip
+}
+
+output "backend_private_ip" {
+  value = module.ec2.backend_private_ip
+}
+
+output "alb_dns_name" {
+  value = module.alb.alb_dns_name
+}
+
+output "rds_endpoint" {
+  value = module.rds.rds_endpoint
+}
+
+output "key_name" {
+  value = aws_key_pair.deployer.key_name
+}
